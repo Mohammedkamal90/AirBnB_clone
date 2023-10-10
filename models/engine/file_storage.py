01bbc25c81b7f6fa5b38028fcb090c78203d9023
HalimaEla59 <halimaelabbadi@gmail.com>
2023-10-10 15:45:57 -0700

    FileStorage done

---
+++ models/engine/file_storage.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""class serializes instances to JSON file & deserializes JSON to instances"""
+import JSON
+from models.base_models import BaseModel
+
+
+class FileStorage:
+    """the FileStorage class"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        if obj:
+            k = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[k] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        js = {}
+        for key, val in self.__objects.items():
+            js[key] = val.to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(js, f)
+
+    def reload(self):
+        """deserializes JSON file to __objects if it exists (__file_path)"""
+        try:
+            with open(self.__file_path, 'r') as f:
+                ob = json.load(f)
+                for key, obj in data.items():
+                    val = eval(obj['__class__'])(**obj)
+                    self.__objects[key] = val
+        except FileNotFoundError:
+            pass
