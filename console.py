4219799d28e32a730a9c912a8f23f48dbbe9560c
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2023-10-11 14:26:23 -0700

    console

--- console.py
+++ console.py
@@ -2,16 +2,21 @@
 """ console """
 
 import cmd
-import models
-from datetime import datetime
-from models.base_model import BaseModel
-from models.place import Place
-from models.review import Review
-from models.city import City
-from models.state import State
-from models.user import User
-import shlex
-from models.amenity import Amenity
 
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program on EOF"""
+        print("")  # Print a new line before exiting
+        return True
+
+    def emptyline(self):
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
