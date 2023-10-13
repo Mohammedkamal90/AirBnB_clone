d75af0f18a5e28a9185523f60fbb77c87e59ace6
HalimaEla59 <halimaelabbadi@gmail.com>
2023-10-13 02:38:38 -0700

    state.py done

---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""State class inheriting from BaseModels"""
+from models.base_models import BaseModel
+
+
+class State(BaseModel):
+    """representing the State"""
+    name = ""
