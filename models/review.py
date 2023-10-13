9478c3c6591823de42167316e0ea7c1a83df96ac
HalimaEla59 <halimaelabbadi@gmail.com>
2023-10-13 02:58:46 -0700

    review.py added

---
+++ models/review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Review class inheriting from BaseModel"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """representing Review"""
+    place_id = ""
+    user_id  = ""
+    text = ""
