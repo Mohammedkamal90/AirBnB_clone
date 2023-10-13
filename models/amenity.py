6cf5edb9c4c0199f79e570b59284282b29b5c47c
HalimaEla59 <halimaelabbadi@gmail.com>
2023-10-13 02:45:12 -0700

    amenity.py added

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/user/bin/python3
+"""Amenity class inheriting from BaseModel"""
+from models.base_models import BaseModel
+
+
+class Amenity(BaseModel):
+    """representing Amenity"""
+    name = ""
