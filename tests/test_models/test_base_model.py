3f2b03675e0cd92804da2c424f89e7492b194ee8
HalimaEla59 <halimaelabbadi@gmail.com>
2023-10-15 16:40:54 -0700

    test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -31,23 +31,28 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(p.total_errors, 0, "fix pep8")
 
     def test_check_functions(self):
+        """tests func"""
         self.assertIsNotNone(BaseModel.__doc__)
         self.assertIsNotNone(BaseModel.save.__doc__)
         self.assertIsNotNone(BaseModel.to_dict.__doc__)
 
     def test_attribute_basemodel(self):
+        """tests attributes"""
         self.assertTrue(hasattr(BaseModel, "__init__"))
         self.assertTrue(hasattr(BaseModel, "save"))
         self.assertTrue(hasattr(BaseModel, "to_dict"))
 
     def test_init(self):
+        """tests init"""
         self.assertTrue(isinstance(self.testBase, BaseModel))
 
     def test_save(self):
+        """tests save"""
         self.testBase.save()
         self.assertNotEqual(self.testBase.created_at, self.testBase.updated_at)
 
     def test_to_dict(self):
+        """tests dict"""
         copy = self.testBase.to_dict()
         self.assertEqual(self.testBase.__class__.__name__, 'BaseModel')
         self.assertIsInstance(copy['created_at'], str)
